ee3a1f38ba21f2c87e916bcb7c8a03bb378e1663
Simen Fivelstad Smaaberg <simensma@gmail.com>
2020-05-24 20:57:35 -0700

    Added food type, quantity, and description to FeedScheduleForm

--- frontend/src/components/feed-schedule-form/FeedScheduleForm.js
+++ frontend/src/components/feed-schedule-form/FeedScheduleForm.js
@@ -2,13 +2,22 @@ import React from "react";
 import * as yup from "yup";
 import { Form, Col, Button } from "react-bootstrap";
 import { Formik } from "formik";
+import FoodTypeSelector from "../food-type-selector/FoodTypeSelector";
 
 const feedSchema = yup.object().shape({
-  eventDate: yup.date().required('Please enter a date'),
+  eventDate: yup.date().required("Please enter a date"),
+  foodType: yup.string().required("Please select a food type"),
+  quantity: yup
+    .number()
+    .required("Please enter number of ducks observed")
+    .min(0)
+    .max(1000),
+  description: yup.string().max(500),
 });
 
 const initialValues = {
   eventDate: new Date(),
+  quantity: 1,
 };
 
 class FeedScheduleForm extends React.Component {
@@ -40,10 +49,63 @@ class FeedScheduleForm extends React.Component {
                   onBlur={handleBlur}
                   isInvalid={errors.eventDate}
                 ></Form.Control>
-                <Form.Control.Feedback type="invalid">{errors.eventDate}</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  {errors.eventDate}
+                </Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
-            
+
+            <Form.Row>
+              <Col md="6">
+                <Form.Row>
+                  <Form.Group as={Col} md="12" controlId="foodType">
+                    <Form.Label>Food Type</Form.Label>
+                    <FoodTypeSelector
+                      name="foodType"
+                      value={values.foodType}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={errors.foodType}
+                    ></FoodTypeSelector>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.foodType}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="12" controlId="quantity">
+                    <Form.Label>Quantity</Form.Label>
+                    <Form.Control
+                      type="number"
+                      name="quantity"
+                      value={values.quantity}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={errors.quantity}
+                    ></Form.Control>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.quantity}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Form.Row>
+              </Col>
+              <Col md="6">
+                <Form.Group as={Col} md="12" controlId="description">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    name="description"
+                    rows="5"
+                    value={values.description}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    isInvalid={errors.description}
+                  ></Form.Control>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.description}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+            </Form.Row>
+
             <Button type="submit">Submit entry</Button>
           </Form>
         )}
--- frontend/src/components/feed-schedule-form/FeedScheduleForm.test.js
+++ frontend/src/components/feed-schedule-form/FeedScheduleForm.test.js
@@ -1,40 +1,100 @@
-import React from 'react';
-import FeedScheduleForm from "./FeedScheduleForm"
-import {render, fireEvent, wait} from '@testing-library/react'
-import { act } from 'react-dom/test-utils';
+import axios from "axios";
 
-describe('when rendering component', () => {
+import React from "react";
+import FeedScheduleForm from "./FeedScheduleForm";
+import { render, fireEvent, wait } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
-    describe('date field', () => {
-        it('should exist', () => {
-            const {getByLabelText} = render(<FeedScheduleForm/>);
-            getByLabelText('Date');
-        });
+import MockAdapter from "axios-mock-adapter";
 
-        it('should not show an error message on load', async () => {
-            const {queryByText} = render(<FeedScheduleForm/>);
-
-            await wait();
-            const input = queryByText('Please enter a date');
-
-            expect(input).toBeNull();
-        });
-
-        it('should show error message when empty', async () => {
-            const {getByText, getByLabelText} = render(<FeedScheduleForm/>);
-            const input = getByLabelText('Date');
-
-            act(() => {
-                fireEvent.change(input, {
-                    target: {
-                        value: '',
-                    }
-                });
-            });
-            
-            await wait();
-            
-            getByText('Please enter a date');
-        });
+describe("when rendering component", () => {
+  describe("date field", () => {
+    it("should exist", () => {
+      const { getByLabelText } = render(<FeedScheduleForm />);
+      getByLabelText("Date");
     });
-})
+
+    it("should not show an error message on load", async () => {
+      const { queryByText } = render(<FeedScheduleForm />);
+
+      await wait();
+      const input = queryByText("Please enter a date");
+
+      expect(input).toBeNull();
+    });
+
+    it("should show error message when empty", async () => {
+      const { getByText, getByLabelText } = render(<FeedScheduleForm />);
+      const input = getByLabelText("Date");
+
+      act(() => {
+        fireEvent.change(input, {
+          target: {
+            value: "",
+          },
+        });
+      });
+
+      await wait();
+
+      getByText("Please enter a date");
+    });
+  });
+
+  describe("quantity field", () => {
+    it("should exist", () => {
+      const { getByLabelText } = render(<FeedScheduleForm />);
+      getByLabelText("Quantity");
+    });
+
+    it("should not show an error message on load", async () => {
+      const { queryByText } = render(<FeedScheduleForm />);
+
+      await wait();
+      const input = queryByText("Please enter number of ducks observed");
+
+      expect(input).toBeNull();
+    });
+
+    it("should show error message when empty", async () => {
+      const { getByText, getByLabelText } = render(<FeedScheduleForm />);
+      const input = getByLabelText("Quantity");
+
+      act(() => {
+        fireEvent.change(input, {
+          target: {
+            value: "",
+          },
+        });
+      });
+
+      await wait();
+
+      getByText("Please enter number of ducks observed");
+    });
+  });
+
+  describe("description field", () => {
+    it("should exist", () => {
+      const { getByLabelText } = render(<FeedScheduleForm />);
+      getByLabelText("Description");
+    });
+
+    it("should show error message when more than 500 chars", async () => {
+      const { getByText, getByLabelText } = render(<FeedScheduleForm />);
+      const input = getByLabelText("Description");
+
+      act(() => {
+        fireEvent.change(input, {
+          target: {
+            value: "t".repeat(501),
+          },
+        });
+      });
+
+      await wait();
+
+      getByText("description must be at most 500 characters");
+    });
+  });
+});
